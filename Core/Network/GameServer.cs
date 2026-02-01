fdf15088e6f92530acaa8aba3086d5de730cc489
Higor Oliveira <higorosantos@gmail.com>
2026-01-31 21:37:13 -0300

    feat: criando api a de login

--- Core/Network/GameServer.cs
+++ Core/Network/GameServer.cs
@@ -6,44 +6,91 @@ using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 using LiteNetLib;
+using LiteNetLib.Utils;
 
 namespace JJK_Project_Server.Core.Network
 {
   class GameServer : INetEventListener
   {
+
+    private NetManager _netManager;
+
+    public void Start(int port)
+    {
+      this._netManager = new NetManager(this);
+      this._netManager.Start(port);
+
+      Console.WriteLine("AI MEU CARALHO");
+    }
+
     public void OnConnectionRequest(ConnectionRequest request)
     {
-      throw new NotImplementedException();
+      request.AcceptIfKey("game_key");
     }
 
     public void OnNetworkError(IPEndPoint endPoint, SocketError socketError)
     {
-      throw new NotImplementedException();
+
+    }
+
+    public void PollEvents()
+    {
+      _netManager.PollEvents();
     }
 
     public void OnNetworkLatencyUpdate(NetPeer peer, int latency)
     {
-      throw new NotImplementedException();
+      Console.WriteLine("Latencia do Usuário -> {0} {1}ms", peer.Address ,latency);
     }
 
-    public void OnNetworkReceive(NetPeer peer, NetPacketReader reader, byte channelNumber, DeliveryMethod deliveryMethod)
+    public void OnNetworkReceive(
+       NetPeer peer,
+       NetPacketReader reader,
+       byte channelNumber,
+       DeliveryMethod deliveryMethod)
     {
-      throw new NotImplementedException();
+      HandlePacket(peer, reader);
+      reader.Recycle();
     }
 
-    public void OnNetworkReceiveUnconnected(IPEndPoint remoteEndPoint, NetPacketReader reader, UnconnectedMessageType messageType)
+    private void HandlePacket(NetPeer peer, NetPacketReader reader)
     {
-      throw new NotImplementedException();
+      string message = reader.GetString();
+      Console.WriteLine("O Cliente me falou -> {0}", message);
+
+      NetDataWriter writer = new NetDataWriter();
+      writer.Put("480ba6feccd5d66a39d172e0bf82d2fa76c7b626");
+
+      peer.Send(writer, DeliveryMethod.ReliableOrdered);
+      // switch packetType ...
     }
 
     public void OnPeerConnected(NetPeer peer)
     {
-      throw new NotImplementedException();
+      Console.WriteLine("Usuário Conectado Endereço: {0}", peer.Address);
     }
 
     public void OnPeerDisconnected(NetPeer peer, DisconnectInfo disconnectInfo)
     {
-      throw new NotImplementedException();
+      Console.WriteLine("Usuário -> {0} Desconetado", peer.Address);
     }
+
+    public void OnNetworkReceiveUnconnected(IPEndPoint remoteEndPoint, NetPacketReader reader, UnconnectedMessageType messageType)
+    {
+    }
+
+    //public void PrintStatsAtTop()
+    //{
+    //  var stats = this._netManager.Statistics;
+
+    //  Console.SetCursorPosition(0, 0);
+    //  Console.WriteLine("--------------------------------------------------");
+    //  Console.WriteLine($"PACKETS LOSS: {stats.PacketLoss} ms");
+    //  Console.WriteLine($"PACKETS SENT: {stats.PacketsSent}");
+    //  Console.WriteLine($"PACKETS RECEIVED: {stats.PacketsReceived}");
+    //  Console.WriteLine($"BYTES SENT: {stats.BytesSent}");
+    //  Console.WriteLine($"BYTES RECEIVED: {stats.BytesReceived}");
+    //  Console.WriteLine("--------------------------------------------------");
+    //}
   }
 }
--- Worker.cs
+++ Worker.cs
@@ -1,23 +1,38 @@
+using JJK_Project_Server.Core.Network;
+
 namespace JJK_Project_Server;
 
 public class Worker : BackgroundService
 {
     private readonly ILogger<Worker> _logger;
+    private GameServer _gameServer;
 
-    public Worker(ILogger<Worker> logger)
+  public Worker(ILogger<Worker> logger)
     {
         _logger = logger;
     }
 
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
-        while (!stoppingToken.IsCancellationRequested)
-        {
-            if (_logger.IsEnabled(LogLevel.Information))
-            {
-                _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
-            }
-            await Task.Delay(1000, stoppingToken);
-        }
+
+    //while (!stoppingToken.IsCancellationRequested)
+    //{
+    //    if (_logger.IsEnabled(LogLevel.Information))
+    //    {
+    //      this._gameServer.PollEvents();
+    //      //_logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
+    //    }
+    //    await Task.Delay((1000 / 30), stoppingToken);
+    //}
+
+    _gameServer = new GameServer();
+    _gameServer.Start(4000);
+
+    while (!stoppingToken.IsCancellationRequested)
+    {
+      //_gameServer.PrintStatsAtTop();
+      _gameServer.PollEvents();
+      await Task.Delay(1000 / 30, stoppingToken);
     }
+  }
 }
